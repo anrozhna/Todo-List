04a0a9825b31f642497a6bb28d04d5d45f8585e2
Anna <annalepiska@gmail.com>
2024-05-30 13:50:39 +0300

    create task_create view, url and template

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from tasks import views
-from tasks.views import IndexView
+from tasks.views import IndexView, TaskCreateView
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from tasks.models import Task
@@ -8,3 +9,9 @@ class IndexView(generic.ListView):
     model = Task
     template_name = "tasks/index.html"
     paginate_by = 3
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
