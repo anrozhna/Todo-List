d039034f3907b8cde7817f8f97dd1dc920a0272c
Anna <annalepiska@gmail.com>
2024-05-30 13:10:14 +0300

    create index view, url and template

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+
+from tasks import views
+from tasks.views import IndexView
+
 app_name = "tasks"
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+]
